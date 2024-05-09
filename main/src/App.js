50f8e186f5e150566f92b8752cca2c445ee4b50f
Supriya Kumari <supriyayout@gmail.com>
2024-05-09 21:26:22 +0530

    todoapp created

--- main/src/App.js
+++ main/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Component/Counter';
+import TodoApp from './Component/TodoApp';
 
 function App() {
   return (
     <div className="App">
    <Counter/>
+   <TodoApp/>
     </div>
   );
 }
--- main/src/Component/TodoApp.jsx
+++ main/src/Component/TodoApp.jsx
@@ -3,13 +3,34 @@ import { useState,useEffect } from 'react'
 function TodoApp() {
 
    const [task,settask]=useState('')
+   const [todo,settodo]=useState([])
 
-   const Todos=[]
+//    const Todos=[]
+
+
+   function addTodo(e){
+    e.preventDefault()
+         const newTodo={task}
+        settodo([...todo,newTodo])
+        settask('')
+   }
+
+   console.log(todo)
+   
 
     return (
         <div>
             <h1>Todo App</h1>
-            
+            <form onSubmit={addTodo}>
+                <input type="text" value={task} onChange={(e)=>settask(e.target.value)} />
+                <button type='submit'>Add Todo</button>
+            </form>
+            {
+
+                todo.map((todo,index)=>(
+                    <li key={index}>{todo.task}</li>
+                ))
+            }
             
         </div>
     )
